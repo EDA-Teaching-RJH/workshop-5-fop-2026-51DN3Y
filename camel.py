cd02657423edcf6f602af313f4f2e38a36e0bfff
Sidney Beacher <163619322+51DN3Y@users.noreply.github.com>
2026-02-08 00:58:05 +0000

    Uncommented

--- camel.py
+++ camel.py
@@ -1,1 +1,17 @@
-# Enter your code here
+def main():
+    camel_case = input("Input Camel Case: ")
+    snake = camel_to_snake(camel_case)
+    print(snake)
+
+
+def camel_to_snake(text):
+    snake = ""
+    for c in text:
+        if c.isupper():
+            snake += "_" + c.lower()
+        else:
+            snake += c
+    return snake
+
+
+main()
--- coffee.py
+++ coffee.py
@@ -1,1 +1,28 @@
-#Your code goes here
+def main():
+    amount_due = 75
+
+    while amount_due > 0:
+        print("Amount due:", amount_due, "p")
+        coin = get_coin()
+        amount_due = update_total(amount_due, coin)
+
+    dispense_product(amount_due)
+
+def get_coin():
+    while True:
+        coin = int(input("Insert coin: "))
+        if coin in [50, 20, 10, 5]:
+            return coin
+        else:
+            print("Enter a correct denomination of either 50p, 20p, 10p, or 5p ")
+
+
+def update_total(current, coin):
+    return current - coin
+
+
+def dispense_product(amount_due):
+    print("Change owed:", -amount_due, "p")
+
+
+main()
